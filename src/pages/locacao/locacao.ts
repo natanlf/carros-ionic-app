9d5fcff0b9d6276453687ea0d00df3d04fb35d88
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-26 11:46:45 -0200

    Pegando cliente da locacao

--- src/pages/locacao/locacao.ts
+++ src/pages/locacao/locacao.ts
@@ -1,6 +1,8 @@
+import { StorageService } from './../../services/storage.service';
+import { ClienteService } from './../../services/domain/cliente.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LocacaoPage page.
@@ -14,35 +16,48 @@ import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/fo
   selector: 'page-locacao',
   templateUrl: 'locacao.html',
 })
+
 export class LocacaoPage {
-
+  carro: any
   formGroup: FormGroup;
-
-  dias: number
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    private clienteService: ClienteService,
+    private storage: StorageService) {
 
       this.formGroup = this.formBuilder.group({
         diasPrevistos: [1, Validators.required], //diaria
         desconto: [0.10, Validators.required], //longo periodo
         instanteDevolucao: [null, Validators.required],
-        "@type": ["LocacaoLongoPeriodo", Validators.required] //diaria oulongo periodo
-        //cliente id
-        //carro_id
-        //sede_id
+        "@type": ["LocacaoLongoPeriodo", Validators.required], //diaria oulongo periodo
+        cliente: {id: 6},  //cliente id
+        carro: {id: 1}, //carro_id
+        sede: {id: 1}//sede_id
       });
   }
 
   ionViewDidLoad() {
-    let carro = this.navParams.get('carro')
-    console.log(carro.id)
+    this.carro = this.navParams.get('carro');
+    this.formGroup.controls.carro.setValue({id: this.carro.id});
+    this.formGroup.controls.sede.setValue({id: this.carro.sede.id});
+
+    let localUser = this.storage.getLocalUser()
+    this.clienteService.findByEmail(localUser.email)
+    .subscribe(response=>{
+      localStorage.setItem("clienteId", response.id.toString())
+      //console.log(this.cli);
+    },error=>{})
+    console.log(localStorage.getItem("clienteId"));
+    console.log(this.carro);
+    this.formGroup.controls.cliente.setValue({id: localStorage.getItem("clienteId")});
+    console.log(JSON.stringify(this.formGroup.value));
   }
 
   saveLocacao(){
-    console.log("save locacao")
+    console.log("save locacao"+ JSON.stringify(this.formGroup.value))
   }
 
 }
