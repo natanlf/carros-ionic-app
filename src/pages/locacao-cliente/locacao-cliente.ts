113b4bd83565e7d83dd65faa64d84fc1b21a24c9
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-29 16:09:02 -0200

    Mostrando  imagens em locacoes

--- src/pages/locacao-cliente/locacao-cliente.ts
+++ src/pages/locacao-cliente/locacao-cliente.ts
@@ -1,6 +1,8 @@
+import { CarroService } from './../../services/domain/carro.service';
 import { LocacaoService } from './../../services/domain/locacao.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the LocacaoClientePage page.
@@ -22,15 +24,28 @@ export class LocacaoClientePage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private locacaoService: LocacaoService) {
+    private locacaoService: LocacaoService,
+    private carroService: CarroService) {
   }
 
   ionViewDidLoad() {
     this.locacaoService.findByCliente(this.page)
     .subscribe(response=>{
       this.items = response['content']
+      this.loadImageUrls()
       console.log(this.items)
       },error=>{})
   }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.carroService.getImageFromBucket(item.carro.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/car${item.carro.id}.jpg`;
+        },
+        error => {});
+    }
+  }  
 
 }
